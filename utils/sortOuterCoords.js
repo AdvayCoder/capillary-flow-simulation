c6b5136806f5d1bb0849a9c1eba2d192d941c910
AdvayCoder <palnitkaradvay@gmail.com>
2023-10-23 08:54:48 +0530

    Fixed outer coords sort logic

--- utils/sortOuterCoords.js
+++ utils/sortOuterCoords.js
@@ -1,12 +1,24 @@
 const sortOuterCoords = (outerCoordsArray, circleRadius) => {
-    const upperArray = []; //upper half of system
-    const lowerArray = []; //lower half of system
+    const firstQaurter = []; //upper half of system
+    const secondQuarter = []; //upper half of system
+    const thirdQuarter = []; //lower half of system
+    const fourthQuarter = []; //lower half of system
 
-    for (let x = circleRadius; x > -circleRadius; x--) {
-        for (let y = 0; y <= circleRadius; y++) {
+    for (let x = circleRadius; x >= 0; x--) {
+        for (let y = 0; y < circleRadius; y++) {
             outerCoordsArray.forEach((coord) => {
                 if (x === coord.x && y === coord.y) {
-                    upperArray.push(coord);
+                    firstQaurter.push(coord);
+                }
+            });
+        }
+    }
+
+    for (let x = 0; x > -circleRadius; x--) {
+        for (let y = circleRadius; y >= 0; y--) {
+            outerCoordsArray.forEach((coord) => {
+                if (x === coord.x && y === coord.y) {
+                    secondQuarter.push(coord);
                 }
             });
         }
@@ -16,13 +28,23 @@ const sortOuterCoords = (outerCoordsArray, circleRadius) => {
         for (let y = 0; y >= -circleRadius; y--) {
             outerCoordsArray.forEach((coord) => {
                 if (x === coord.x && y === coord.y) {
-                    lowerArray.push(coord);
+                    thirdQuarter.push(coord);
                 }
             });
         }
     }
 
-    return upperArray.concat(lowerArray).map((coord, coordIndex) => {
+    for (let x = 0; x < circleRadius; x++) {
+        for (let y = -circleRadius; y >= 0; y++) {
+            outerCoordsArray.forEach((coord) => {
+                if (x === coord.x && y === coord.y) {
+                    fourthQuarter.push(coord);
+                }
+            });
+        }
+    }
+
+    return firstQaurter.concat(secondQuarter, thirdQuarter, fourthQuarter).map((coord, coordIndex) => {
         return { ...coord, index: coordIndex };
     }); //index is given for user to be able to easily chose the coords for side water flow
 };
